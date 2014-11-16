a39ca06cc808c49bd3a9e3350627871303dc11ad
tamblc <tamblc@rpi.edu>
2014-11-15 21:02:08 -0500

    Current Working version

--- BuzzOff.safariextension/listener.js
+++ BuzzOff.safariextension/listener.js
@@ -1,12 +1,57 @@
-function blocker(burl, recursive){
-	$(".fsm, .fwn, .fcg:contains(buzzfeed)").insertBefore('<!--').insertAfter('-->')
+function buzzoff(){
+
+
+	var stories = document.getElementsByClassName("fsm fwn fcg");
+	var story;
+	alert("called buzzoff");
+	for(var i=0; i < stories.length; i++){
+		story = stories[i];
+		if (story.value.toLowerCase().indexOf("buzzfeed.com") !== -1){
+			alert("called story");
+	    	killStories(story);
+    	}
+	}
+
+	stories = document.getElementsByClassName("_4-u2 mbm _5jmm _5pat _5v3q _5x16 _x72");
+	for(var i=0; i < stories.length; i++){
+		story = stories[i];
+		if (story.value.toLowerCase().indexOf("buzzfeed.com") !== -1){
+	    	killStories(story);
+    	}
+	}
+
+	stories = document.getElementsByClassName("_4-u2 mbm _5jmm _5pat _5v3q _5x16");
+	for(var j=0; i < stories.length; i++){
+		story = stories[i];
+		if (story.value.toLowerCase().indexOf("buzzfeed.com") !== -1){
+	    	killStories(story);
+    	}
+	}
+
+	stories = document.getElementsByClassName("timelineUnitContainer pagesTimelinePinpost");
+	for(var k=0; i < stories.length; i++){
+		story = stories[i];
+		if (story.value.toLowerCase().indexOf("buzzfeed.com") !== -1){
+	    	killStories(story);
+    	}
+	}
+
 }
 
+function killStories(item){
+	alert("called");
+	// var elems = item.getElementsByClassName("fsm fwn fcg");
+	item.style.opacity = "0.0";
+	 item.style.display = "None";
 
-/*
-bind clickHandler function for each 
-existing element of the document.
-clickHandler will trigger when user
-click on a element.
-*/
-$(document).on("click",clickHandler);
+	// var post;
+	// for(var i=0; i < elems.length; i++){
+	//     post = stories[i];
+
+	// }
+}
+alert(document.title;)
+
+if (document.title.toLowerCase().indexOf("about:: blank")!==-1){
+	buzzoff();
+}
